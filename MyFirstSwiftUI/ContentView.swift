59de987a989273cab5f213ea21bcbd3c8501bec6
ugur.narman <Ugur*1373>
2021-07-26 18:10:55 +0300

    Initial Commit

---
+++ MyFirstSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirstSwiftUI
+//
+//  Created by Uğur Narman on 26.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFirstSwiftUI/MyFirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUIApp.swift
+//  MyFirstSwiftUI
+//
+//  Created by Uğur Narman on 26.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
